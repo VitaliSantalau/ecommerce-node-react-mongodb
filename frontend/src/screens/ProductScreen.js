6b649e545165b1ce2cbd70de1cbd1a2c0bfeb8f8
VitaliSantalau <v.santalau@gmail.com>
2021-03-25 23:43:24 +0300

    next

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Rating from '../components/Rating'
 import data from '../data'
 
 
@@ -13,10 +14,28 @@ export default function ProductScreen(props) {
     <div>
       <div className="row">
         <div className="col-2">
-          <img src={product.image} alt={product.name}/>
+          <img className="large" src={product.image} alt={product.name}/>
         </div>
-        <div className="col-1"></div>
-        <div className="col-1"></div>
+        <div className="col-1">
+          <ul>
+            <li><h1>{ product.name }</h1></li>
+            <li><Rating rating={product.rating} numReviews={product.numReviews} /></li>
+            <li>Price : ${product.price}</li>
+            <li><p>DEscription : {product.description}</p></li>
+          </ul>
+        </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+              <li>
+                <div className="row">
+                  <div>Price: </div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
