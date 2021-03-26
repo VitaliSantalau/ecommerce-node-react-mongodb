d279aabb2a06fd7e7638d188a8bf0cb6d54a4eef
VitaliSantalau <v.santalau@gmail.com>
2021-03-26 11:14:57 +0300

    productscreen ready

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Rating from '../components/Rating'
 import data from '../data'
 
@@ -12,7 +13,8 @@ export default function ProductScreen(props) {
 
   return (
     <div>
-      <div className="row">
+      <Link to="/">Back to result</Link>
+      <div className="row top">
         <div className="col-2">
           <img className="large" src={product.image} alt={product.name}/>
         </div>
@@ -21,7 +23,7 @@ export default function ProductScreen(props) {
             <li><h1>{ product.name }</h1></li>
             <li><Rating rating={product.rating} numReviews={product.numReviews} /></li>
             <li>Price : ${product.price}</li>
-            <li><p>DEscription : {product.description}</p></li>
+            <li><p>Description : {product.description}</p></li>
           </ul>
         </div>
         <div className="col-1">
@@ -32,6 +34,21 @@ export default function ProductScreen(props) {
                   <div>Price: </div>
                   <div className="price">${product.price}</div>
                 </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Status </div>
+                  <div>
+                    {
+                      product.countInStock > 0 ? 
+                        <span className="success">In Stock</span> : 
+                        <span className="danger">Unavailable</span>
+                    }
+                  </div>
+                </div>
+              </li>
+              <li>
+                <button className="primary block">Add to Cart</button>
               </li>
             </ul>
           </div>
