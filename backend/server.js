575e2e5c65c2ac50f0c2f54583ed968a3c84b02a
VitaliSantalau <v.santalau@gmail.com>
2021-03-28 23:14:20 +0300

    add redux

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,21 @@ app.get('/api/products', (req, res) => {
   res.send(data.products)
 })
 
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find(item => item._id === req.params.id)
+  if(product) {
+    res.send(product)
+  } else {
+    res.status(404).send({message: "Product not Found"})
+  }
+  
+})
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message })
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`)
 })
