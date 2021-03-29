814b33356d7e42cc0d4ef7dac14862d4babb1c83
VitaliSantalau <v.santalau@gmail.com>
2021-03-29 17:17:55 +0300

    create add to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom'
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 
-
 export default function App() {
+
+  const cart = useSelector(state => state.cart)
+  const { cartItems } = cart
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a href="/" className="logo">e-commerce</a>
+            <Link to="/" className="logo">e-commerce</Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign in</a>
+            <Link to="/cart">Cart
+              {
+                cartItems.length > 0 && (
+                  <span className="badge">{cartItems.length}</span>
+                )
+              }
+            </Link>
+            <Link to="/signin">Sign in</Link>
           </div>
         </header>
         <main>
           <Route path="/" component={HomeScreen} exact></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
         </main>
         <footer className="row">
           All right reserved
